051460bef59f4096147a55f2e47e38a372f80d34
Yug1275 <yjpatel1275@gmail.com>
2026-03-11 11:40:13 +0530

    Phase 2 completed: React setup, Tailwind, Express server, MongoDB Atlas connection

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">
+        Yug Patel Portfolio
+      </h1>
+    </div>
+  );
+}
+
+export default App;
