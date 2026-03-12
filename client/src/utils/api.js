af45fc2653ef4b58cd2b0c10cf8914c54fdc9db0
Yug1275 <yjpatel1275@gmail.com>
2026-03-12 23:38:43 +0530

    Fix API URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://yug-portfolio-backend.onrender.com"
+  baseURL: "https://yug-portfolio-backend.onrender.com/api"
+  // baseURL: "http://localhost:5000/api"
 });
 
 export default API;
