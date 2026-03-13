ee41a04a530799481d690348a7d63eebac8c233c
Yug1275 <yjpatel1275@gmail.com>
2026-03-13 10:10:14 +0530

    Fix Botpress chatbot loading

--- client/src/components/Chatbot.js
+++ client/src/components/Chatbot.js
@@ -2,24 +2,20 @@ import { useEffect } from "react";
 
 const Chatbot = () => {
   useEffect(() => {
-    // Botpress inject script
-    const script1 = document.createElement("script");
-    script1.src = "https://cdn.botpress.cloud/webchat/v3.6/inject.js";
-    script1.async = true;
+    const injectScript = document.createElement("script");
+    injectScript.src = "https://cdn.botpress.cloud/webchat/v3.6/inject.js";
+    injectScript.async = true;
 
-    // Your bot configuration script
-    const script2 = document.createElement("script");
-    script2.src =
-      "https://files.bpcontent.cloud/2026/03/13/04/20260313040503-1C3NZITT.js";
-    script2.defer = true;
+    injectScript.onload = () => {
+      const botScript = document.createElement("script");
+      botScript.src =
+        "https://files.bpcontent.cloud/2026/03/13/04/20260313040503-1C3NZITT.js";
+      botScript.defer = true;
 
-    document.body.appendChild(script1);
-    document.body.appendChild(script2);
-
-    return () => {
-      document.body.removeChild(script1);
-      document.body.removeChild(script2);
+      document.body.appendChild(botScript);
     };
+
+    document.body.appendChild(injectScript);
   }, []);
 
   return null;
